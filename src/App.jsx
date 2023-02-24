02836ee6a72b2c67eb988f1b3484c4c23ddc2df5
Guilherme de Souza Melo <guilherme.melo10@yahoo.com.br>
2023-02-24 15:06:04 -0300

    Finalizando Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+import "./app.css";
 
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametros';
 import RandomNumber from "./components/basicos/RandomNumber";
 import Card from "./components/layouts/Card";
 
-const forte = <h1> <strong>Hello World! I'm using JSX</strong></h1>;
 
 export default () => (
-  <div>
-    { forte }
+  <div className="app">
     <h1>Fundamentos React</h1>
-    <Card titulo="#03 - Desafio número aleatório">
-      <RandomNumber numIni={1} numFim={10} />
-    </Card>
-
-    <Card titulo="#02 - Testando parâmetros">
-      <ComParametro titulo="Análise dos alunos" nome="Joãozinho" nota={8.0} />
-    </Card>
-
-    <Card titulo="#01 - Primeiro componente">
-      <Primeiro />
-    </Card>
+    <div className="cards">
+      <Card titulo="#03 - Desafio número aleatório" color="#090">
+        <RandomNumber numIni={1} numFim={10} />
+      </Card>
+      <Card titulo="#02 - Testando parâmetros" color="#F9CB11">
+        <ComParametro titulo="Análise dos alunos" nome="Joãozinho" nota={8.0} />
+      </Card>
+      <Card titulo="#01 - Primeiro componente" color="#F08080">
+        <Primeiro />
+      </Card>
+    </div>
     
   </div>
 );
